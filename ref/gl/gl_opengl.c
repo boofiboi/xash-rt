9064c0242408b2eaa43e679a3940113897e1c6ee
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-07-31 17:11:39 +0200

    rt: translucent brush parts

--- ref/gl/gl_opengl.c
+++ ref/gl/gl_opengl.c
@@ -2692,6 +2692,7 @@ static void TryBeginBatch( RgUtilImScratchTopology glbegin_topology )
 			.pPrimitiveNameInMesh = rt_state.curTexture2DName, // NULL,
 			.primitiveIndexInMesh = rt_state.curBrushSurface,
 			.flags                = ( rt_alphatest ? RG_MESH_PRIMITIVE_ALPHA_TESTED : 0 ) |
+									( rt_raster_blend ? RG_MESH_PRIMITIVE_TRANSLUCENT : 0 ) |
 									( rt_state.curBrushSurfaceIsWater ? RG_MESH_PRIMITIVE_WATER : 0 ),
 			.pTextureName         = rt_state.curTexture2DName,
 			.textureFrame         = 0,
