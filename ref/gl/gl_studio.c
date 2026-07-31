e5fa342d42c1a2348faa5ca07523864988f3e2da
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-07-31 19:39:52 +0200

    rt: enable chrome texcoords only in classic; loosely match frozen chrome texcoords in RT

--- ref/gl/gl_studio.c
+++ ref/gl/gl_studio.c
@@ -1122,7 +1122,10 @@ static void R_StudioSetupChrome( float *pchrome, int bone, vec3_t normal )
 {
 	if( g_studio.chromeage[bone] != g_studio.framecount )
 	{
-#if !XASH_RAYTRACING
+#if XASH_RAYTRACING
+		if( RT_CVAR_TO_BOOL( rt_classic ) )
+		{
+#endif
 		// calculate vectors from the viewer to the bone. This roughly adjusts for position
 		vec3_t	chromeupvec;	// g_studio.chrome t vector in world reference frame
 		vec3_t	chromerightvec;	// g_studio.chrome s vector in world reference frame
@@ -1142,9 +1145,13 @@ static void R_StudioSetupChrome( float *pchrome, int bone, vec3_t normal )
 		Matrix3x4_VectorIRotate( g_studio.lighttransform[bone], chromeupvec, g_studio.chromeup[bone] );
 		Matrix3x4_VectorIRotate( g_studio.lighttransform[bone], chromerightvec, g_studio.chromeright[bone] );
 
-#else
-		VectorSet( g_studio.chromeup[ bone ], 0, 0, 1 );
-		VectorSet( g_studio.chromeright[ bone ], 0, 1, 0 );
+#if XASH_RAYTRACING
+		}
+		else
+		{
+		VectorSet( g_studio.chromeup[ bone ], 0, 0, -1 );
+		VectorSet( g_studio.chromeright[ bone ], 0, -1, 0 );
+		}
 #endif
 
 		g_studio.chromeage[bone] = g_studio.framecount;
