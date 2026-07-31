50c30020dfc7a663275e5b6b6742349d7283adaf
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-07-31 17:03:19 +0200

    rt: tweaks + latest rtgl mapname/bloom

--- ref/gl/gl_opengl.c
+++ ref/gl/gl_opengl.c
@@ -1224,7 +1224,7 @@ static void GL_InitCommands( void )
 	CVAR_DEF_T( rt_normalmap_stren,			"1",	"" )
 	CVAR_DEF_T( rt_emis_mapboost,			"30",	"" )
 	CVAR_DEF_T( rt_emis_maxscrcolor,		"32",	"" )
-	CVAR_DEF_T( rt_emis_fullbright_dflt,	"32",	"" )
+    CVAR_DEF_T( rt_emis_additive_dflt,		"0.01",	"" )
 
 	CVAR_DEF_T( rt_sky,						"1",	"sky intensity")
 	CVAR_DEF_T( rt_sky_saturation,			"1",	"sky saturation")
@@ -1247,15 +1247,16 @@ static void GL_InitCommands( void )
 	CVAR_DEF_T( rt_texture_nearest,			"1",	"nearest texture filter for the world" )
 	CVAR_DEF_T( rt_particles_notex,			"0",	"don't use texture for particles" )
 
-	CVAR_DEF_T( rt_volume_type,				"2",	"" )
+	CVAR_DEF_T( rt_volume_type,				"1",	"0 - none, 1 - volumetric, 2 - distance based" )
 	CVAR_DEF_T( rt_volume_far,				"1000",	"" )
 	CVAR_DEF_T( rt_volume_scatter,			"0.3",	"" )
 	CVAR_DEF_T( rt_volume_ambient,			"0.5",	"" )
 	CVAR_DEF_T( rt_volume_lintensity,		"1",	"" )
 	CVAR_DEF_T( rt_volume_lassymetry,		"0.75",	"" )
 
-	CVAR_DEF_T( rt_bloom_intensity,			"1",	"bloom intensity" )
-	CVAR_DEF_T( rt_bloom_emis_mult,			"0",	"bloom multiplier for emissive" )
+    CVAR_DEF_T( rt_bloom_intensity,			"1",	"bloom intensity" )
+    CVAR_DEF_T( rt_bloom_threshold,			"4",	"bloom threshold" )
+    CVAR_DEF_T( rt_bloom_emis_mult,			"9",	"bloom multiplier for emissive" )
 	
 	CVAR_DEF_T( rt_ef_crt,					"0",	"CRT-monitor filter" )
     CVAR_DEF_T( rt_ef_chraber,				"0.3",	"chromatic aberration intensity" )
@@ -2578,7 +2579,9 @@ static void TryBeginBatch( RgUtilImScratchTopology glbegin_topology )
 			.pTextureName = rt_state.curTexture2DName,
 			.textureFrame = 0,
 			.color        = rgUtilPackColorByte4D( 255, 255, 255, 255 ),
-			.emissive     = rt_raster_blend && rt_raster_additive ? 1.0f : 0.0f,
+            .emissive     = rt_raster_blend && rt_raster_additive
+                                ? RT_CVAR_TO_FLOAT( rt_emis_additive_dflt )
+                                : 0.0f,
 			.pEditorInfo  = NULL,
 		};
 
@@ -2604,7 +2607,9 @@ static void TryBeginBatch( RgUtilImScratchTopology glbegin_topology )
 			.pTextureName = rt_state.curTexture2DName,
 			.textureFrame = 0,
 			.color        = rgUtilPackColorByte4D( 255, 255, 255, 255 ),
-			.emissive     = rt_raster_blend && rt_raster_additive ? 1.0f : 0.0f,
+            .emissive     = rt_raster_blend && rt_raster_additive
+                                ? RT_CVAR_TO_FLOAT( rt_emis_additive_dflt )
+                                : 0.0f,
 			.pEditorInfo  = NULL,
 		};
 
