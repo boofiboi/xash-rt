b2b389379672e05a3c07e442063e61877a8b9860
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-07-31 19:42:20 +0200

    rt: dont export water

--- ref/gl/gl_opengl.c
+++ ref/gl/gl_opengl.c
@@ -2809,9 +2809,10 @@ static void TryBeginBatch( RgUtilImScratchTopology glbegin_topology )
 			.uniqueObjectID = RI.currententity->index,
 			.pMeshName      = RI.currentmodel->name,
 			.transform      = MATRIX4_TO_RGTRANSFORM( RI.objectMatrix ),
-			.isExportable   = immovable && !rt_state.curBrushSurfaceIsAnimated,
-			.animationName  = NULL,
-			.animationTime  = 0.0f,
+			.isExportable   = immovable && !rt_state.curBrushSurfaceIsAnimated &&
+							!rt_state.curBrushSurfaceIsWater,
+			.animationName = NULL,
+			.animationTime = 0.0f,
 		};
 
 		RgMeshPrimitiveInfo prim = {
