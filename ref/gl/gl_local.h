3d160f55412193c90a6c61c31c5293b4c22da320
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-07-31 20:58:50 +0200

    rt: egon lights

--- ref/gl/gl_local.h
+++ ref/gl/gl_local.h
@@ -809,6 +809,7 @@ extern rt_state_t rt_state;
 	void RT_ParseStaticLightEntities( void );
 void RT_UploadAllLights( void );
 void RT_TryAddParticleLight( const cl_entity_t* e );
+void RT_TryAddBeamLight( vec3_t pos, uint8_t r, uint8_t g, uint8_t b );
 void RT_BindLightmapTexture( int texnum );
 void RT_ResetChapterLogo( void );
 void RT_TryFindTramLights( void );
@@ -866,6 +867,7 @@ typedef struct rt_cvars_t
 	rt_light_d,
 	rt_light_s,
 	rt_light_p,
+	rt_light_b,
 	rt_light_tram,
 	rt_light_tram_h,
 	rt_light_tram_ao,
