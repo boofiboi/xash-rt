8b4a0e3e989a866bb76610d21618a4183e0916ac
boofiboi <79476279+boofiboi@users.noreply.github.com>
2026-07-31 15:22:15 +0200

    rt: remove draw elements, only use fixed function

--- ref/gl/gl_opengl.c
+++ ref/gl/gl_opengl.c
@@ -1163,7 +1163,9 @@ static void GL_InitCommands( void )
 	gEngfuncs.Cvar_RegisterVariable( &r_lockfrustum );
 	gEngfuncs.Cvar_RegisterVariable( &r_traceglow );
 	gEngfuncs.Cvar_RegisterVariable( &r_studio_sort_textures );
+#if !XASH_RAYTRACING
 	gEngfuncs.Cvar_RegisterVariable( &r_studio_drawelements );
+#endif
 	gEngfuncs.Cvar_RegisterVariable( &r_ripple );
 	gEngfuncs.Cvar_RegisterVariable( &r_ripple_updatetime );
 	gEngfuncs.Cvar_RegisterVariable( &r_ripple_spawntime );
